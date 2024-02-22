4dd8d03eafbd01b6a73634a03550552cc5fa7e98
Islom <mirislom1234@gmail.com>
2024-02-22 21:49:00 +0500

    upgrade

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 "use strict";
 
 
+
+
 function darkmode() {
     const body = document.body
     const wadDarkmode = localStorage.getItem('darkmode') === 'true'
-
 
     localStorage.setItem('darkmode', !wadDarkmode)
     body.classList.toggle('dark-mode', !wadDarkmode)
